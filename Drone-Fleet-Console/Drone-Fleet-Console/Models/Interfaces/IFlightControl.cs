ab56b0ee5a2ab7a81c554f8c87c1580fcf63ca2c
Ciurchil-Giurgiu Celia <Celia.Ciurchil@endava.com>
2025-10-16 17:02:31 +0300

    simple interfaces for base class Drone

---
+++ Drone-Fleet-Console/Drone-Fleet-Console/Models/Interfaces/IFlightControl.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Fleet_Console.Models.Interfaces
+{
+    interface IFlightControl
+    {
+        void TakeOff();
+        void Land();
+    }
+}
---
+++ Drone-Fleet-Console/Drone-Fleet-Console/Models/Interfaces/ISelfTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Fleet_Console.Models.Interfaces
+{
+    interface ISelfTest
+    {
+        bool RunSelfTest();
+    }
+}
