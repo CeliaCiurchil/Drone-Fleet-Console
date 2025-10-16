86949e83554a55419e9e8493df2ca3f70aecd0ef
Ciurchil-Giurgiu Celia <Celia.Ciurchil@endava.com>
2025-10-16 17:03:11 +0300

    added drone base abstract class implementation with id generation

---
+++ Drone-Fleet-Console/Drone-Fleet-Console/Models/Drone.cs
@@ -0,0 +1,42 @@
+﻿using Drone_Fleet_Console.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Fleet_Console.Models
+{
+    abstract class Drone : IFlightControl, ISelfTest
+    {
+        public Drone()
+        {
+            DroneId = s_nextDroneId++;
+        }
+        static Drone()
+        {
+            Random rand = new Random();
+            s_nextDroneId = rand.Next(1000, 9999);
+        }
+        public int DroneId { get; }
+        public string? Name { get; set; }
+        public int BatteryPercentage { get; set; }
+        public bool isAirborne { get; set; }
+        static int s_nextDroneId = 1;
+
+        public void TakeOff()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Land()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RunSelfTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
