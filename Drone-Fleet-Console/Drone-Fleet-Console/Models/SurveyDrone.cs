164c7d32e7439564402f7e6e747ad435201a8829
Ciurchil-Giurgiu Celia <ciurchilgiurgiucelia@yahoo.com>
2025-10-20 15:55:36 +0300

    SurveyDrone implementation

---
+++ Drone-Fleet-Console/Drone-Fleet-Console/Models/SurveyDrone.cs
@@ -0,0 +1,55 @@
+﻿using Drone_Fleet_Console.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drone_Fleet_Console.Models
+{
+    internal class SurveyDrone : Drone, IPhotoCapture, INavigable
+    {
+        public int PhotoCount { get; private set; }
+
+        public Coordinates? CurrentWaypoint { get; private set; }
+        public SurveyDrone()
+        {
+            PhotoCount = 0;
+            Name = "Survey Drone";
+        }
+
+        public void SetWaypoint(Coordinates coordinates)
+        {
+            CurrentWaypoint = coordinates;
+        }
+
+        public void TakePhoto()
+        {
+            if (!isAirborne)
+            {
+                Console.WriteLine("Cannot take photo: Drone is not airborne. Please take off!");
+                return;
+            }
+            PhotoCount++;
+            Console.WriteLine($"Photo taken. Total photos: {PhotoCount}");
+        }
+
+        internal override void GetActions()
+        {
+            Console.WriteLine($"Actions for {Name}:");
+            Console.WriteLine("1. Take Photo");
+        }
+        internal override void PerformAction(int? option = null)
+        {
+            switch (option)
+            {
+                case 1:
+                    TakePhoto();
+                    break;
+                default:
+                    Console.WriteLine("Invalid action for Survey Drone.");
+                    break;
+            }
+        }
+    }
+}
